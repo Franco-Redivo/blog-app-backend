5f405f4ea2be76cc31b0676bde4a32c195e6ac7c
Franco Redivo <franco161101@gmail.com>
2026-05-28 14:43:58 -0300

    feat: implement CRUD operations for blogs with Sequelize integration

--- index.js
+++ index.js
@@ -3,7 +3,96 @@ const express = require('express');
 const { Sequelize, Model, DataTypes } = require('sequelize');
 
 const app = express();
+app.use(express.json());
 
+const sequelizeOptions = process.env.DB_SSL === 'true'
+    ? {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
+    : {};
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, sequelizeOptions);
+
+class Blog extends Model {}
+Blog.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    author: {
+        type: DataTypes.STRING,
+    },
+    url: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+}, { 
+    sequelize, 
+    modelName: 'blog', 
+    timestamps: false, 
+    underscored: true
+})
+
+Blog.sync();
+
+app.get('/api/blogs', async (req,res) => {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+})
+
+app.post('/api/blogs', async (req,res) => {
+    try {
+        const blog = await Blog.create(req.body);
+        res.status(201).json(blog);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+app.put('/api/blogs/:id', async (req,res) => {
+    try {
+        const blog = await Blog.findByPk(req.params.id);
+        if(blog) {
+            await blog.update(req.body);
+            res.json(blog);
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+})
+
+app.get('/api/blogs/:id', async (req,res) => {
+    const blog = await Blog.findByPk(req.params.id);
+    if(blog) {
+        res.json(blog);
+    } else {
+        res.status(404).json({ error: 'Blog not found'});
+    }
+})
+
+app.delete('/api/blogs/:id', async (req,res) => {
+    const blog = await Blog.findByPk(req.params.id);
+    if(blog) {
+        await blog.destroy();
+        res.status(204).end();
+    } else {
+        res.status(404).json({ error: 'Blog not found'});
+    }
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
