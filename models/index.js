1ddb8cfdaa5be12d95dda432336ad7e1c878ac6c
Franco Redivo <franco161101@gmail.com>
2026-06-01 21:56:02 -0300

    feat: establish associations between Blog and User models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,13 @@
 const Blog = require('./blog');
+const User = require('./user');
 
-Blog.sync();
+Blog.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Blog, { foreignKey: 'userId' });
+
+//Blog.sync({ alter: true });
+//User.sync({ alter: true });
 
 module.exports = {
-    Blog
+    Blog,
+    User
 }
